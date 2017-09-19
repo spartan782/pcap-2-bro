25f2a50ff929e3d81a81bc400636141cca870382
John <spartan782@gmail.com>
2017-09-18 19:14:31 -0600

    Added source option with default UUID to track source of data. updating formatting to new Python string format.

--- pcap-2-rock.py
+++ pcap-2-rock.py
@@ -1,4 +1,5 @@
 from functools import partial
+from uuid import uuid4
 
 import subprocess
 import argparse
@@ -14,22 +15,23 @@ import time
 def set_logging():
     # Set logging level. Log to directory script was run from as __file__.stderr
     logging_level = logging.INFO  # Modify if you just want to focus on errors
-    logging.basicConfig(filename='%s/%s.stderr' % (os.path.dirname(os.path.abspath(__file__)), os.path.basename(__file__)),
-                        level=logging_level,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        stream=sys.stdout)
+    logging.basicConfig(filename='{}/{}.stderr'.format(
+        os.path.dirname(os.path.abspath(__file__)), os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0]),
+        level=logging_level,
+        format='{asctime} - {levelname:8s}: {message}',
+        datefmt='{Y}-{m}-{d} {H}:{M}:{S}',
+        stream=sys.stdout)
 
 
 def get_pcap_files_recursive(top_dir):
     pcap_files = []
     for root, dirs, files in os.walk(top_dir):
         for file_name in files:
-            pcap_files.append('%s%s' % (root, file_name))
+            pcap_files.append('{}{}'.format(root, file_name))
     if pcap_files:
         return pcap_files
     else:
-        print 'No files exist in %s' % top_dir
+        print 'No files exist in {}'.format(top_dir)
         exit(1)
 
 
@@ -39,7 +41,7 @@ def get_pcap_files(directory):
     if pcap_files:
         return pcap_files
     else:
-        print 'No files exist in %s' % directory
+        print 'No files exist in {}'.format(directory)
         exit(1)
 
 
@@ -50,7 +52,7 @@ def get_pcap_file(file_name):
         pcap_files = [file_path]
         return pcap_files
     else:
-        print '%s Is not a File' % file_path
+        print '{} Is not a File'.format(file_path)
         exit(1)
 
 
@@ -62,6 +64,7 @@ def get_args():
     parser.add_argument('-v', '--verbose', action="store_true", help='Enable Verbose output', default=False)
     parser.add_argument('-f', '--file', help='process a single Pcap file')
     parser.add_argument('-w', '--workers', type=int, help='Number of Parallel workers to process pcap', default=1)
+    parser.add_argument('-s', '--source', type=int, help='Used to supply the ROCK::sensor_id variable', default=uuid4())
 
     args = parser.parse_args()
 
@@ -69,6 +72,9 @@ def get_args():
     if args.directory and args.file:
         parser.error('Can only use one of the following options [-d,-f]')
 
+    # Check for at least one valid option
+    if not args.directory and not args.file:
+        parser.error('Must supply either one of the following option [-d,-f]')
     return args
 
 
@@ -97,16 +103,16 @@ def get_bro_executable():
                 exit(1)
 
 
-def run_bro_replay(pcap, bro_path):
+def run_bro_replay(pcap, bro_path, source):
     try:
         # Bro command to replay pcap
-        command = '%s -C -r %s local' % (bro_path, pcap)
+        command = '{} -C -r {} local "ROCK::sensor_id={}"'.format(bro_path, pcap, source)
         # Run the command
         subprocess.call(shlex.split(command), stdout=subprocess.PIPE)
     except Exception:
-        print 'Failed to process %s' % pcap
-        print 'See %s.stderr for more details' % os.path.dirname(os.path.abspath(__file__))
-        logging.error('Failed to process %s\n%s', pcap, traceback.format_exc())
+        print 'Failed to process {}'.format(pcap)
+        print 'See {}.stderr for more details'.format(os.path.splitext(__file__)[0])
+        logging.error('Failed to process {}\n{}'.format(pcap, traceback.format_exc()))
 
 
 def main():
@@ -136,13 +142,14 @@ def main():
     pool = multiprocessing.Pool(processes=workers)
 
     # map the fuction and send the data needed for the function
-    result = pool.map_async(partial(run_bro_replay, bro_path=bro_path), pcap_list)
+    result = pool.map_async(partial(run_bro_replay, bro_path=bro_path, source=args.source), pcap_list)
     if args.verbose:
         while not result.ready():
-            print 'Pcap Left to Process: %s' % result._number_left
+            print 'Pcap Left to Process: {}'.format(result._number_left)
             time.sleep(10)
     pool.close()
     pool.join()
+    print 'Pcap processed. Rock'
 
 
 main()
